fd1edd17ee6a612a476bda14ec9ea34cbd4cc4ab
EstebanCajamarca <cajamare@oregonstate.edu>
2022-01-31 22:39:59 -0600

    .

---
+++ multiply.py
@@ -0,0 +1,17 @@
+# Author: Esteban Cajamarca
+# GitHub username: EstebanCajamarca
+# Date: 1/31/2022
+#
+# Write a recursive function named
+# multiply that takes two positive integers as parameters and returns the product of those two numbers (the result
+# from multiplying them together). Your program should not use multiplication - it should find the result by using
+# addition.
+
+def multiplication(multiplicand, multiplier):
+    """Gives multiplication value by using addition method."""
+    # Multiplier is different from one.
+    if multiplier == 1:
+        # If multiplier is one, the result will be the multiplicand itself.
+        return multiplicand
+    # performs multiplication by addition.
+    return multiplication(multiplicand, multiplier-1) + multiplicand
